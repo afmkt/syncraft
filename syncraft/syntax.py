c6fec708930ec38af5f89117b55e759809b5f6f4
michael <afmkt1020@proton.me>
2025-08-24 21:26:31 -0500

    bug fix

--- syncraft/syntax.py
+++ syncraft/syntax.py
@@ -18,30 +18,6 @@ C = TypeVar('C')  # Result type for else branch
 S = TypeVar('S')  # State type
 
 
-class Variable(Generic[A]):
-    def __init__(self, name: Optional[str] = None):
-        self.name = name
-        self.initialized = False
-        self._value = None
-    def __call__(self, a: Any) -> Any:
-        if self.initialized:
-            if self._value != a:
-                raise ValueError(f"Variable {self.name or ''} is already initialized")
-            else:
-                return a
-        elif self.name is not None and isinstance(a, NamedResult):
-            if a.name == self.name:
-                self._value = a.value
-                self.initialized = True
-        else:
-            self._value = a
-            self.initialized = True
-        return a
-    @property
-    def value(self) -> A:
-        if self.initialized is False or self._value is None:
-            raise ValueError(f"Variable {self.name or ''} is not initialized")
-        return self._value
 
 
 @dataclass(frozen=True)
