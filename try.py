d3de7d1bb0162c732609c038017790b7d6bd4583
michael <afmkt1020@proton.me>
2026-03-25 12:46:00 +0800

    bug fix

--- try.py
+++ try.py
@@ -16,9 +16,9 @@ def test_generate_with_direct_left_recursion_with_base_succeeds():
     # A := A + 'a' | 'a'
     a = SS.tok('a').named('a')
     A = SS.lazy(lambda: (A + a) | a).named('A')  # type: ignore[name-defined]
-    print("START PARSING", '=='*200)
+    print("START PARSING", '=='*80)
     P = A.parse(['a'])
-    print("START GENERATING", '=='*200)
+    print("START GENERATING", '=='*80)
     ast = generate_with(A)
     # Should yield an AST (not Error) and produce a bindings mapping (possibly empty)
     assert not isinstance(ast, Error)
